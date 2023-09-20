3847bb583e6c7685b7b7e22ddf007575874d6b84
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-09-20 19:05:00 +0300

    Initial commit

---
+++ AnimationBorder/AnimationBorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationBorderApp.swift
+//  AnimationBorder
+//
+//  Created by Iaroslav Beldin on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationBorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
